050da5006c58f24a1fd134ef29fb3e60e92a766b
mugju <ddaa1155k@gmail.com>
2021-12-22 02:21:19 +0900

    pre setting

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-Parser");
 const passport = require("passport");
+const morgan = require("morgan");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const authRouter = require("./routes/auth");
+
