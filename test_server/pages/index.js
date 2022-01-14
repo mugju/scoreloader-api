0c1c83e2cec158dac9f6f85df5eefca86cdd8f70
mugju <ddaa1155k@gmail.com>
2022-01-14 19:38:45 +0900

    api client ok

--- test_server/pages/index.js
+++ test_server/pages/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const axios = require('axios');
+const { verifyToken } = require('../../routes/middlewares');
 
 const router = express.Router();
 
@@ -40,6 +41,40 @@ router.get('/test',async (req,res,next)=>{
         // console.log(res.json(tokenTest.data));
         console.log('Test 완료');
         return res.json(tokenTest.data);
+    }
+    catch(err){
+        console.error(err);
+        return next(err);
+
+    }
+});
+
+
+router.get('/hashtag/:tag', async (req,res,next) =>{
+   
+    try{
+        if(!req.session.jwt){ //없는 경우 발급받는 로직.
+            console.log('api 서버에 접속중...'); //확인용 500error 문제있음.
+            const jwtToken = await axios.post('http://localhost:9001/v1/token',{
+                clientSecret : process.env.CLIENT_SECRET  //이거 스펠링 req.body에 들어갈거
+            });
+            console.log('내용나올거임..',jwtToken.data); //키값좀 확인해보자 //키값이 안뜸.
+            if (jwtToken.data && jwtToken.data.code == 200){ //토큰 발급성공임.
+                req.session.jwt = jwtToken.data.token;
+            }
+            else{
+                console.log('토큰 문제있음');
+                
+                return res.json(jwtToken.data);
+            }
+        } //여기까지가 원래 토큰 받는 로직.
+        console.log('정상적 실행');
+        const result = await axios.get(`http://localhost:9001/v1/posts/hashtag/${encodeURIComponent(req.params.tag)}`,{
+            headers:{
+                authorization : req.session.jwt //위의 주석코드 때문
+            }
+        });
+        res.json(result.data);
     }
     catch(err){
         console.error(err);
